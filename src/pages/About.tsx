a95f74f0e809b6686bac234870848c9bf3bcbca6
STACY <stacy.imagines888@passinbox.com>
2026-01-26 15:20:05 +0100

    created footer

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,27 +1,11 @@
 import Header from "../components/header";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Link as RouterLink } from "react-router-dom";
+import Footer from "../components/footer";
 
 export default function About() {
 	return (
-		<Box sx={{ my: 4 }}>
+		<>
 			<Header />
-			<Typography variant="h4" component="h1" gutterBottom>
-				À propos
-			</Typography>
-			<Typography variant="body1" gutterBottom>
-				Cette page est un exemple de navigation avec React Router.
-			</Typography>
-			<Typography variant="body1" paragraph>
-				React Router permet de naviguer entre différentes pages de votre
-				application sans recharger la page complète.
-			</Typography>
-
-			<Button variant="contained" component={RouterLink} to="/">
-				Retour à l'accueil
-			</Button>
-		</Box>
+			<Footer />
+		</>
 	);
 }
