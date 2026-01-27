3fa033b21ffed781368081068a051ed3e5c93544
STACY <stacy.imagines888@passinbox.com>
2026-01-27 11:18:51 +0100

    Final MVP version of Header & Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,20 +3,28 @@ import { Link as RouterLink } from "react-router-dom";
 export default function Footer() {
     return (
         <footer
-            className="h-[267px] absolute bottom-0 left-0 right-0 flex items-center"
+            className="h-[167px] absolute bottom-0 left-0 right-0 flex items-center"
             style={{ backgroundColor: "#185227" }}
         >
             <nav className="m-auto pr-[20px] flex gap-4">
-                <RouterLink to="/" className="footerLink absolute right-10 bottom-10">
-                    Accueil
+                <p className="text-white absolute left-5 bottom-5">
+                    © 2024 Strasbooq. All rights reserved.</p>
+                <RouterLink to="/Accueil">
+                    <img
+                        src="src/assets/footerLogo.png"
+                        alt="Footer Logo"
+                        className="absolute bottom-10 left-0 right-0 m-auto h-[100px]"
+                        style={{ filter: "drop-shadow(2px 5px 4px rgba(0, 0, 0, 0.3))" }}
+                    ></img>
+                </RouterLink>
+                <RouterLink to="/Contact" className="text-white absolute right-5 bottom-5 mt-5">
+                    Contact
+                </RouterLink>
+                <RouterLink to="/Adresse" className="text-white absolute right-5 bottom-15 mt-5">
+                    Adresse
                 </RouterLink>
             </nav>
-            <img
-                src="src/assets/footerLogo.png"
-                alt="Footer Logo"
-                className="absolute bottom-10 left-0 right-0 m-auto h-[100px]"
-                style={{ filter: "drop-shadow(2px 5px 4px rgba(0, 0, 0, 0.3))" }}
-            ></img>
+
         </footer>
     );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,18 +7,27 @@ export default function Header() {
         <header
             className="h-[100px] relative flex items-center"
             style={{ backgroundColor: "rgba(158, 42, 43, 0.6)" }}>
-            <img
-                src="src/assets/logo.png"
-                alt="Logo"
-                className="absolute left-[20px] top-[20px] h-[120px]"
-                style={{ filter: "drop-shadow(2px 5px 4px rgba(0, 0, 0, 0.3))" }}
-            />
+
             <nav className="ml-auto pr-[20px] flex gap-4">
-                <RouterLink to="/" className="headerLink">
-                    Accueil
+                <RouterLink to="/Accueil" className="HomePage">
+                    <img
+                        src="src/assets/logo.png"
+                        alt="Logo"
+                        className="absolute left-[20px] top-[20px] h-[120px]"
+                        style={{ filter: "drop-shadow(2px 5px 4px rgba(0, 0, 0, 0.3))" }}
+                    />
                 </RouterLink>
-                <RouterLink to="/about" className="headerLink">
-                    À propos
+                <RouterLink to="/ChoisirBouquet" className="ChoisirBouquet">
+                    Choisir un bouquet
+                </RouterLink>
+                <RouterLink to="/CreerBouquet" className="CreerBouquet">
+                    Créer un bouquet
+                </RouterLink>
+                <RouterLink to="/Panier" className="Panier">
+                    <img src="src/assets/Panier.png" alt="Panier" className="h-[30px]" />
+                </RouterLink>
+                <RouterLink to="/Favoris" className="Favoris">
+                    <img src="src/assets/Favoris.png" alt="Favoris" className="h-[30px]" />
                 </RouterLink>
             </nav>
         </header>
