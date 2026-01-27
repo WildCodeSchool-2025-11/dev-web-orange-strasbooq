c4cd96c2bd662ca740ff48a76aaa0fdba4f6c7d4
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-01-27 17:25:03 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/footer.tsx";
+import Header from "./components/header.tsx";
 import ChoisirBouquet from "./pages/ChoisirBouquet";
 import ChoisirFleurs from "./pages/ChoisirFleurs";
 import HomePage from "./pages/HomePage";
-import Header from "./components/header.tsx";
-import Footer from "./components/footer.tsx";
 import "./App.css";
 
 export default function App() {
 	return (
-
 		<>
-		<Header />
+			<Header />
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/choisir-bouquet" element={<ChoisirBouquet />} />
 				<Route path="/choisir-fleurs" element={<ChoisirFleurs />} />
 			</Routes>
-		<Footer />
+			<Footer />
 		</>
 	);
 }
