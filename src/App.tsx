7ec0a299f1789e5a0a0c91843f228a96bb408de9
Prescillia Rosart <prescillia.rosart@gmail.com>
2026-02-06 10:13:19 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -20,20 +20,20 @@ export default function App() {
 			<CartProvider>
 				<CustomBouquetProvider>
 					<div className="min-h-screen flex flex-col">
-					<Header />
-					<main className="flex-1">
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/choisir-bouquet" element={<ChoisirBouquet />} />
-						<Route path="/choisir-fleurs" element={<ChoisirFleurs />} />
-						<Route path="/Panier" element={<MonPanier />} />
-						<Route path="/Favoris" element={<PageFavoris />} />
-						<Route path="/conseils" element={<Conseils />} />
-						<Route path="/contact" element={<Contact />} />
-						<Route path="/admin/dashboard" element={<DashBoard />} />
-					</Routes>
-					</main>
-					<Footer />
+						<Header />
+						<main className="flex-1">
+							<Routes>
+								<Route path="/" element={<HomePage />} />
+								<Route path="/choisir-bouquet" element={<ChoisirBouquet />} />
+								<Route path="/choisir-fleurs" element={<ChoisirFleurs />} />
+								<Route path="/Panier" element={<MonPanier />} />
+								<Route path="/Favoris" element={<PageFavoris />} />
+								<Route path="/conseils" element={<Conseils />} />
+								<Route path="/contact" element={<Contact />} />
+								<Route path="/admin/dashboard" element={<DashBoard />} />
+							</Routes>
+						</main>
+						<Footer />
 					</div>
 				</CustomBouquetProvider>
 			</CartProvider>
